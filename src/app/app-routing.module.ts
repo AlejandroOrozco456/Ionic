f63ba37895ea93900a5b56ae5633b83c79ee9d47
AlejandroOrozco456 <alejandrogobass@gmail.com>
2022-02-25 11:14:35 -0600

    Practica Tinder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { PublicacionesComponent } from './components/publicaciones/publicaciones.component';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path:'galeria',
+    component:GaleriaComponent
+  },
+  {
+    path:'publica',
+    component:PublicacionesComponent
   }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { PublicacionesComponent } from './components/publicaciones/publicaciones.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+  HeaderComponent,
+  GaleriaComponent,
+  PublicacionesComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
